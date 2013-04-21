dfd58bc270382562f05b68314ee6d76fbc416687
sajana <agnosco11@gmail.com>
2013-04-21 09:55:27 +0545

    PHP for uploading hex file

---
+++ ArduSAT/arfile.php
@@ -0,0 +1,42 @@
+<?php 
+if(isset($_POST['submit'])) {
+
+	$data= $_POST;
+	$action = $_GET['action'];
+	$submit = $data['submit'];
+
+	$arduino = $data['arduino'];
+	$email = $data['email'];
+
+		if($arduino == 1){  //1 for uno
+		$upload_dir = 'uploads/uno';
+		}
+		else{
+		$upload_dir = 'uploads/freeduino';
+		}
+		
+		//tmp_name holds temporary file for uploaded file
+		$source_file = $_FILES['hexfile']['tmp_name'];
+	
+		//the 'name' key of the array holds original filename
+		//we can use original file name here as our destination filename. it will be saved inside our upload directory
+		$destination_file = time()."_".$email."_".$_FILES['hexfile']['name'];	
+		
+		$ext = substr($_FILES['hexfile']['name'], -3);		
+		
+		if ($ext == "hex"){		
+			if (move_uploaded_file($source_file, $upload_dir."/".$destination_file)){
+			$file_error = "File Uploaded Successfully";			
+				//file upload done;
+			}else {
+				$hasError = true;
+				$file_error = "Couldn't upload file. Retry later";
+			}		
+		}else {
+				$hasError = true;
+				$file_error = "Only Hex files are allowed";		
+		}
+		
+	}
+?>
+	
---
+++ ArduSAT/index.php
@@ -0,0 +1,56 @@
+<?php
+	include "arfile.php";
+?>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>ArduSAT</title>
+		<link href="css/style.css" rel="stylesheet">
+		<style type="text/css">
+		body 
+			{
+			background-image:url('css/images/background.jpg');
+			/*background-color:#cccccc;*/
+			}
+</style>
+	</head>
+	
+	<body>
+	<div class="container">
+	<form class="form-horizontal" action="<?php echo $_SERVER['PHP_SELF'];?>" method="POST" enctype="multipart/form-data" >
+		<div class="control-group">
+		<label class="control-label">Arduino Type</label>
+		<div class="controls">
+		<select name="arduino" id="arduino"  value="">
+              <option value="1">uno</option>
+			  <option value="2">FREEDUINO</option>
+        <select>
+		</div>
+		</div>
+		<br>
+		<div class="control-group">
+		<label class="control-label">Hex file </label>
+		<div class="controls">
+		<input type="file" id="hexfile" name="hexfile" value="" >
+		</div>
+		<br>
+		<?php if (isset($file_error)) echo "*".$file_error; ?>
+		</div>
+		<div class="control-group">
+		<label class="control-label">Email</label>
+		<div class="controls">
+		<input type="email" id="email" name="email">
+		<br>
+		*Enter your Email for response
+		</div>
+		</div>
+		<br>
+		
+		 <input type="submit" class="btnupload" name="submit" value="Upload">
+		 
+	</form>
+	</div>
+	</body>
+</html>	
+
